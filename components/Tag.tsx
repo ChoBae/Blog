7c35aab8b0ec3ba1975499fede1af627676365ba
Cho Sung Bae <101175828+ChoBaeKR@users.noreply.github.com>
2024-01-03 14:31:58 +0900

    [fix] 태그 UI

--- components/Tag.tsx
+++ components/Tag.tsx
@@ -8,7 +8,7 @@ const Tag = ({ text }: Props) => {
   return (
     <Link
       href={`/tags/${slug(text)}`}
-      className="mr-3 text-sm font-medium uppercase text-primary-500 hover:text-primary-600 dark:hover:text-primary-400"
+      className="mr-3 mt-2 rounded-lg border border-primary-500 px-3 py-1 text-sm font-medium uppercase text-primary-500 hover:text-primary-600 dark:hover:text-primary-400"
     >
       {text.split(' ').join('-')}
     </Link>
