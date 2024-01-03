732794dff3358de7a5f33c0f2d8e4200960b70a1
Cho Sung Bae <101175828+ChoBaeKR@users.noreply.github.com>
2024-01-03 15:37:03 +0900

    [fix] 이미지 엑박 해결

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -2,19 +2,19 @@ const projectsData = [
   {
     title: 'DIA',
     description: `DIA는 개발자 면접 도우미 입니다. 취업,이직을 위한 코딩테스트 앱은 많지만 면접 준비를 위한 앱은 없다는 것에서 시작했습니다.개발자들이 좀더 실전적인 면접 연습을 할수 있게 돕는것이 목표입니다.`,
-    imgSrc: '/static/images/dia.png',
+    imgSrc: '/static/images/projects/dia.png',
     href: '/blog/projects/dia-interview-assistant',
   },
   {
     title: 'SummaryWagon',
     description: `OPENAI 활용 사이트 요약 서비스`,
-    imgSrc: '/static/images/summarywagon.png',
+    imgSrc: '/static/images/projects/summarywagon.png',
     href: '/blog/projects/summarywagon',
   },
   {
     title: 'Highlighters',
     description: `사이트의 중요한 부분을 빠르게 파악하고 공유할 수 있는 유용한 협업 툴`,
-    imgSrc: '/static/images/highlighters.png',
+    imgSrc: '/static/images/projects/highlighters.png',
     href: '/blog/projects/highlighters',
   },
 ]
