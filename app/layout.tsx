ff7b9ced0431f25d2f94e379a913279d6eac1e3f
Cho Sung Bae <101175828+ChoBaeKR@users.noreply.github.com>
2024-01-07 20:59:24 +0900

    [feat]naver search

--- app/layout.tsx
+++ app/layout.tsx
@@ -76,6 +76,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
       <meta name="msapplication-TileColor" content="#000000" />
       <meta name="theme-color" media="(prefers-color-scheme: light)" content="#fff" />
       <meta name="theme-color" media="(prefers-color-scheme: dark)" content="#000" />
+      <meta name="naver-site-verification" content="a3f0597777f63f66ef7df2fca522e8699ed79525" />
       <link rel="alternate" type="application/rss+xml" href="/feed.xml" />
       <body className="bg-white text-black antialiased dark:bg-gray-950 dark:text-white">
         <ThemeProviders>
